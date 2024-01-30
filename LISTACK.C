b4231dea57eeb3391f6904f738d7fa9396e00aac
Malavika <146753226+malvikaaaaa@users.noreply.github.com>
2024-01-30 23:23:32 +0530

    Add files via upload

--- LISTACK.C
+++ LISTACK.C
@@ -5,7 +5,7 @@
 void push();
 void pop();
 void display();
-
+void search();
 
 struct node{
 int data;
@@ -21,7 +21,7 @@ void main()
     do
     {
 
-	printf("SELECT\n1.PUSH\n2.POP\n3.Display\n4.Exit\nEnter your choice:");
+	printf("SELECT\n1.PUSH\n2.POP\n3.Display\n4.Search\n5.Exit\nEnter your choice:");
 	scanf("%d", &ch);
 
 	switch (ch)
@@ -31,6 +31,8 @@ void main()
 		case 2: pop();
 			break;
 		case 3: display();
+			break;
+		case 4: search();
 			break;
 		default:exit(0);
 	}
@@ -51,19 +53,19 @@ void push()
     // if list is empty
     if (top->data == NULL)
     {
-        top = malloc(sizeof(struct node));
-        top->data = data;
-        top->next = NULL;
-        printf("Node Inserted\n");
+	top = malloc(sizeof(struct node));
+	top->data = data;
+	top->next = NULL;
+	printf("Node Inserted\n");
     }
     // if list contain atleast one element
     else
     {
-        newnode = malloc(sizeof(struct node));
-        newnode->data = data;
-        newnode->next = top;
-        top = newnode;
-        printf("Node Inserted\n");
+	newnode = malloc(sizeof(struct node));
+	newnode->data = data;
+	newnode->next = top;
+	top = newnode;
+	printf("Node Inserted\n");
     }
 }
 
@@ -73,19 +75,19 @@ void pop()
 
     if (top == NULL)
 
-        printf("\nNo Element to Delete\n\n");
+	printf("\nNo Element to Delete\n\n");
 
     else if (top->next == NULL)
     {
-        printf("\nThe node deleted is %d\n\n", top->data);
-        top = NULL;
+	printf("\nThe node deleted is %d\n\n", top->data);
+	top = NULL;
     }
     else
     {
-        printf("\nThe node deleted is %d\n\n", top->data);
-        temp=top;
-        top = top->next;
-        free(temp);
+	printf("\nThe node deleted is %d\n\n", top->data);
+	temp=top;
+	top = top->next;
+	free(temp);
     }
 }
 
@@ -100,14 +102,53 @@ void display()
     else
     {
 
-        curr = top;
+	curr = top;
 	printf("\nThe Stack is \n");
-        while (curr != NULL)
-        {
+	while (curr != NULL)
+	{
 	    printf("%d\n", curr->data);
-            curr = curr->next;
-        }
+	    curr = curr->next;
+	}
     }
 
     printf("\n\n");
+}
+
+void search()
+{
+	struct node *curr;
+	int flag,value,pos;
+	printf("\nEnter the value to be searched:");
+	scanf("%d",&value);
+	if(top==NULL)
+		printf("\nStack empty");
+	else if(top->next==NULL)
+	{
+		if(top->data==value)
+			printf("\nNode found at position 1");
+		else
+			printf("\nNode not found");
+	}
+	else
+	{
+		flag=0;
+		pos=0;
+		curr=top;
+		while(curr!=NULL)
+		{
+			pos++;
+			if(curr->data==value)
+			{
+				printf("Element found at position %d\n",pos);
+				flag=1;
+				break;
+			}
+			else
+			{
+				curr=curr->next;
+			}
+		}
+	if(flag==0)
+		printf("Element not found\n");
+	}
 }
