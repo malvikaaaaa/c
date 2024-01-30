44383ee9784f7948343fb0e42962d16b4005d2a0
Malavika <146753226+malvikaaaaa@users.noreply.github.com>
2024-01-30 19:53:05 +0530

    Update DISJOINT.C

--- DISJOINT.C
+++ DISJOINT.C
@@ -3,13 +3,25 @@
 
 void unionAB(int a[10],int b[10]);
 int find(int a[10],int b[10]);
+int i,j,k,n,m;
 
 void main()
 {
-	int a[10]={1,2,3,4,5};
-	int b[10]={6,7,8,9};
-	int parent;
+	//int a[10]={1,2,3,4,5};
+	//int b[10]={6,7,8,9};
+	int parent,a[10],b[10];
 	clrscr();
+	printf("\nEnter the no:of elements in set a:");
+	scanf("%d",&n);
+	printf("\nEnter the elements of set a:");
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
+
+	printf("\nEnter the no:of elements in set b:");
+	scanf("%d",&m);
+	printf("\nEnter the elements of set b:");
+	for(i=0;i<m;i++)
+		scanf("%d",&b[i]);
 
 	 unionAB(a,b);
 	 parent=find(a,b);
@@ -25,32 +37,32 @@ void main()
  }
  void unionAB(int a[10],int b[10])
  {
-	int i,j,k,unionAB[10];
+	int unionAB[10];
 	i=0;
-	for(j=0;j<5;j++)
+	for(j=0;j<n;j++)
 	{
 		unionAB[i]=a[j];
 		i++;
 
 	}
-	for(k=0;k<4;k++)
+	for(k=0;k<m;k++)
 	{
 		unionAB[i]=b[k];
 		i++;
 	}
 
 	printf("A -  ");
-	for(i=0;i<5;i++)
+	for(i=0;i<n;i++)
 	{
 		printf("%d ",a[i]);
 	}
 	printf("\nB -  ");
-	for(i=0;i<4;i++)
+	for(i=0;i<m;i++)
 	{
 		printf("%d ",b[i]);
 	}
 	printf("\nAUB -  ");
-	for(i=0;i<9;i++)
+	for(i=0;i<n+m;i++)
 	{
 		printf("%d ",unionAB[i]);
 	}
@@ -60,17 +72,17 @@ void main()
 
  int find(int a[10],int b[10])
  {
-	int num,i;
+	int num;
 	printf("\nEnter the number to be searched\n");
 	scanf("%d",&num);
-	for(i=0;i<5;i++)
+	for(i=0;i<n;i++)
 	{
 		if(a[i]==num)
 		{
 			return a[0];
 		}
 	}
-	for(i=0;i<4;i++)
+	for(i=0;i<m;i++)
 	{
 		if(b[i]==num)
 		{
