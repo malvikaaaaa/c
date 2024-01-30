dc33d6b6207ded2d649ea93ddba1b927ce1527bd
Malavika <146753226+malvikaaaaa@users.noreply.github.com>
2024-01-31 00:42:05 +0530

    Add files via upload

--- CIRCULAR.C
+++ CIRCULAR.C
@@ -4,6 +4,7 @@ int rear=-1,front=-1,q[10],item,n;
 void enqueue();
 void dequeue();
 void display();
+void search();
 
 void main()
 {
@@ -12,7 +13,7 @@ void main()
 	printf("Enter the size of queue:");
 	scanf("%d",&n);
 	do
-	{       printf("1.Enqueue\n2.Dequeue\n3.Display\n4.Exit\nEnter your choice:");
+	{       printf("1.Enqueue\n2.Dequeue\n3.Display\n4.Search\n5.Exit\nEnter your choice:");
 		scanf("%d",&ch);
 		switch(ch)
 		{
@@ -22,7 +23,9 @@ void main()
 				break;
 			case 3: display();
 				break;
-			case 4: exit(0);
+			case 4: search();
+				break;
+			case 5: exit(0);
 			default:printf("Invalid choice\n");
 		}
 	} while(ch!=0);
@@ -80,17 +83,31 @@ void display()
 	else
 	{
 		printf("The circular queue is:\n");
-		for(i=0;i<n;i++)
+		for(i=front;i<=rear;i++)
 		{
-			if(q[i]==NULL)
-			{
-				printf("_\n");
-			}
-			else
-			{
-				printf("%d",q[i]);
-			}
+			printf(" %d",q[i]);
 		}
 	printf("\n");
 	}
+}
+
+void search()
+{
+	int value,flag,pos,i;
+	printf("\nEnter the value to be searched:");
+	scanf("%d",&value);
+	flag=0;
+	pos=0;
+	for(i=front;i<=rear;i++)
+	{
+		pos++;
+		if(q[i]==value)
+		{
+			printf("Element found at position %d\n",pos);
+			flag=1;
+			break;
+		}
+	}
+	if(flag==0)
+		printf("\nElement not found\n");
 }
